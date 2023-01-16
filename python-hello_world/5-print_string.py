8ac439f9d489443dab541d3476e9e1b8b1ae543b
jlucbilo@gmail.com <jlucbilo@gmail.com>
2023-01-16 02:21:53 -0800

    A function that prints 3 times a string stored in the variable str, followed by its first 9 characters.

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str, "\n", str[9], "\n")
-print(str, "\n", str[9], "\n")
-print(str, "\n", str[9], "\n")
+print(str, "\n", str[0:9], "\n")
+print(str, "\n", str[0:9], "\n")
+print(str, "\n", str[0:9], "\n")
