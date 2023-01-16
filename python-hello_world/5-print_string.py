5268ce8f95d4acf1cc887254879a912fe453fcbc
jlucbilo@gmail.com <jlucbilo@gmail.com>
2023-01-16 05:42:07 -0800

    restruction of the code, for printing anothor way

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str, "\n", str[0:9], "\n")
-print(str, "\n", str[0:9], "\n")
-print(str, "\n", str[0:9], "\n")
+print(str, str, str)
+print(str[0:9])
