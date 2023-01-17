eb6338818c54bbded5c1acab293154dd27f5773e
jlucbilo@gmail.com <jlucbilo@gmail.com>
2023-01-17 03:48:53 -0800

    fixing Output because of one missed caracterwq

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for dizaine in range(0, 10):
     for unité in range(dizaine + 1, 10):
         if dizaine == 8 and unité == 9:
-            print("{}{}".format(dizaine, unité), end="")
+            print("{}{}".format(dizaine, unité), end=" ")
         else:
             print("{}{}".format(dizaine, unité), end=", ")
