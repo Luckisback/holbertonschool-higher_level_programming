f9ea2eb8b00aaa37589229d0c0a5f37089b9ef17
Luckisback <jlucbilo@gmail.com>
2023-01-18 22:14:31 +0100

    changing the methode of import, to have just a part

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0 as sum
+from add_0 import add
 if __name__ == "__main__":
 	a = 1
 	b = 2
-	ad = sum.add(a, b)
+	ad = add(a, b)
 	print("{} + {} = {}".format(a, b, ad))
