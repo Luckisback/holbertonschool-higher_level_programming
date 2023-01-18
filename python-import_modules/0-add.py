29951620b25655551236e302bda1ef161837dd4a
Luckisback <jlucbilo@gmail.com>
2023-01-18 15:08:33 +0100

    integration of the condition 'if _NAME...'

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import add_0 as sum
-a = 1
-b = 2
-ad = sum.add(a, b)
-print("{} + {} = {}".format(a, b, ad))
+if __name__ == "__main__":
+	a = 1
+	b = 2
+	ad = sum.add(a, b)
+	print("{} + {} = {}".format(a, b, ad))
