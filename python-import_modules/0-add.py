eb32ee1ed0d7b9b930cffd9fce06f5d33e972b78
Luckisback <jlucbilo@gmail.com>
2023-01-18 22:18:18 +0100

    fixing an error: 'W191 indentation contains tabs'

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
 if __name__ == "__main__":
-	a = 1
-	b = 2
-	ad = add(a, b)
-	print("{} + {} = {}".format(a, b, ad))
+    a = 1
+    b = 2
+    ad = add(a, b)
+    print("{} + {} = {}".format(a, b, ad))
