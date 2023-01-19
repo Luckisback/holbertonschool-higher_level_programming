752824a502aeb857bf2a882f3b230491cfa4801a
Luckisback <jlucbilo@gmail.com>
2023-01-19 14:17:54 +0100

    insertion of an expresion for a particular call of the script

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
-_nb = (len(argv) - 1)
-if _nb == 0:
-    print("{} arguments.".format(_nb))
-if _nb == 1:
-    print("{} argument:".format(_nb))
-if _nb > 1:
-    print("{} arguments:".format(_nb))
-for i in range(1, (_nb + 1)):
-    print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    _nb = (len(argv) - 1)
+    if _nb == 0:
+        print("{} arguments.".format(_nb))
+    if _nb == 1:
+        print("{} argument:".format(_nb))
+    if _nb > 1:
+        print("{} arguments:".format(_nb))
+    for i in range(1, (_nb + 1)):
+        print("{}: {}".format(i, argv[i]))
