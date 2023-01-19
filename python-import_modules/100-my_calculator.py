3d983f6a55c594afb9549a141707f5d5f96f0dd0
Luckisback <jlucbilo@gmail.com>
2023-01-19 22:38:52 +0100

    A program that imports all functions from a file, and handles basic operations.

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+from calculator_1 import (add, sub, mul, div)
+from sys import argv
+
+if __name__ == "__main__":
+
+    lg = len(argv)
+    operator = ["+", "-", "*", "/"]
+    if (lg - 1) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        oper = argv[2]
+    if oper not in operator:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+
+    match oper:
+        case "+":
+            print("{} {} {} = {}".format(a, oper, b, add(a, b)))
+        case "-":
+            print("{} {} {} = {}".format(a, oper, b, sub(a, b)))
+        case "*":
+            print("{} {} {} = {}".format(a, oper, b, mul(a, b)))
+        case "/":
+            print("{} {} {} = {}".format(a, oper, b, div(a, b)))
