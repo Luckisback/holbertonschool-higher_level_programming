731d6168314098b5b5ac3eac92bf55156f0094df
Luckisback <jlucbilo@gmail.com>
2023-01-23 16:44:10 +0100

    New approach to code processing

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    idx = -1
-    for i in range(len(my_list)):
-        elif if my_list[idx] != int(my_list[idx]):
+    for idx in range(len(my_list))
+        if my_list[idx] != int(my_list[idx]):
             pass
-        print("{:d}".format(my_list[idx]))
-        idx += -1
+        print("{:d}".format(my_list[:-1]))
