8784eb3c8735ff708357937c5e5c0c0a2babee71
Luckisback <jlucbilo@gmail.com>
2023-01-23 18:31:57 +0100

    managing the cas of 'length = 0'

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 def print_reversed_list_integer(my_list=[]):
     idx = -1
     for i in range(len(my_list)):
+        if len(my_list) == 0
+            return
         if my_list[idx] != int(my_list[idx]):
             pass
         print("{:d}".format(my_list[idx]))
