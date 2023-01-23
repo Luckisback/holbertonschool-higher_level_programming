f4758c0f0164a298c64c5989a3fa8e668c57ced8
jlucbilo@gmail.com <jlucbilo@gmail.com>
2023-01-23 03:19:50 -0800

    fixing the problem of wrong 2 steps

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or (my_list[idx] == idx):
-        return (None)
-    elif idx > len(my_list) or idx == len(my_list):
-        return (None)
-    elif idx >= 0 and idx <= len(my_list):
+    if idx < 0:
+        return
+    elif idx not in range(len(my_list)):
+        return
+    else:
         return my_list[idx]
