e614c6ff2f7845cb225ffef32987a7546f19463d
Luckisback <jlucbilo@gmail.com>
2023-01-23 16:14:24 +0100

    deletind of an unused block

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,8 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     idx = -1
     for i in range(len(my_list)):
-        if my_list == None
-            return
         elif if my_list[idx] != int(my_list[idx]):
             pass
         print("{:d}".format(my_list[idx]))
