7c9b0e76b544ba62e2aa092585abf50c9518dacc
Luckisback <jlucbilo@gmail.com>
2023-01-23 15:53:57 +0100

    Managing the case of 'list could None'

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 def print_reversed_list_integer(my_list=[]):
     idx = -1
     for i in range(len(my_list)):
+        if my_list = None
+            return
         if my_list[idx] != int(my_list[idx]):
             pass
         print("{:d}".format(my_list[idx]))
