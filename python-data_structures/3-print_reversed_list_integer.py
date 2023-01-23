74f5a0d27b9d2dd646feccfe14d18c829a17bfc1
Luckisback <jlucbilo@gmail.com>
2023-01-23 18:47:41 +0100

    fixing the problem of whitespace around operator

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    idx = -1
-    if my_list ==None:
+    idx = - 1
+    if my_list == None:
         return
     for i in range(len(my_list)):
         if my_list[idx] != int(my_list[idx]):
