cfba20f7026a61c30be83d3e3320c5bd5c76b84b
Luckisback <jlucbilo@gmail.com>
2023-01-23 18:44:46 +0100

    managing the cas of 'list = None'

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     idx = -1
+    if my_list ==None:
+        return
     for i in range(len(my_list)):
-        if my_list == None:
-            return
-        elif my_list[idx] != int(my_list[idx]):
+        if my_list[idx] != int(my_list[idx]):
             pass
         print("{:d}".format(my_list[idx]))
         idx = idx - 1
