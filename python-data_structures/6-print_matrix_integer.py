cc6836abfe8c50b88b61d7b8513f33a38d9a7f3a
Luckisback <jlucbilo@gmail.com>
2023-01-25 17:23:52 +0100

    fixing bad indentationwq

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
         return
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" "\
-                        if j < len(matrix[i])-1 else "\n")
+            print("{:d}".format(matrix[i][j]), end=" "\ 
+            if j < len(matrix[i])-1 else "\n")
