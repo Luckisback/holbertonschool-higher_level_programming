0acdc092b33b8cb836dd6b5b212472a44a9d54e5
Luckisback <jlucbilo@gmail.com>
2023-01-25 18:29:12 +0100

    Dealing with the others 'if conditions'

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list is None:
-        retun my_list
-    elif idx < 0:
+    if idx < 0:
         return my_list
     elif idx > len(my_list):
         return my_list
-    elif my_list == []:
-        return mylist
     elif idx == len(my_list):
         return my_list
     else:
