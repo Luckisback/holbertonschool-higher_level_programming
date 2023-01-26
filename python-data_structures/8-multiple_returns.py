50589f5ff4980873fe1528c1f7a6b516f3afeb05
Luckisback <jlucbilo@gmail.com>
2023-01-26 09:37:32 +0100

    Dealing the condition in what, 'sentence = '

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,9 +2,7 @@
 def multiple_returns(sentence):
     lg = len(sentence)
     fchar = sentence[0]
-    if sentence is None
-        return
-    elif sentence = ""
+    if sentence = ""
         fchar = None
     else:
         tpl = (lg, fchar)
