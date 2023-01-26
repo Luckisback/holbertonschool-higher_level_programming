4679ca8f1fc6b817a5dbfe72d425f72c479c46f0
Luckisback <jlucbilo@gmail.com>
2023-01-26 13:22:32 +0100

    fixing another 'if conditions'

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,6 +6,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     for i in range(len(tuple_a)):
         if len(tpl_a) > len(tpl_b):
             tpl_b.append(0)
+        elif len(tpl_a) < len(tpl_b):
+	    tpl_a.append(0)
         tplist.append(tpl_a[i] + tpl_b[i])
     new_tpl = tuple(tplist)
     return new_tpl
