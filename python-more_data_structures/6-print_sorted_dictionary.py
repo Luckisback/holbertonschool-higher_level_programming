d1933bb322c454c2bc979767d99c39a1963e7b9a
Luckisback <jlucbilo@gmail.com>
2023-01-30 22:48:35 +0100

    dealing with of all the str types to be print

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for i in sorted(a_dictionary.items()):
-        if type(i) == str:
-            print("{}".format(i))
+    for key, value in sorted(a_dictionary.items()):
+            print("{}: {}".format(key, value))
