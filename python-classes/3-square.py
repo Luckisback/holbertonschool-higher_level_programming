1c8c79dce51460fe631e631ee0a547e0e0f0d490
Luckisback <jlucbilo@gmail.com>
2023-02-01 19:18:09 +0100

    A programm that creat a class called square, and includes a function that return a result

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Creation of of a class called Square """
+
+
+class Square:
+    """Definition of the Class Square """
+    def __init__(self, size: int = 0):
+        if type(size) != int or type(size) is None:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Definition of a public instance methode
+            @param seilf: the current instance
+            @Return: returns the current area
+        """
+        return (self.__size)
