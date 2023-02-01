7f6ff7685d154766e8a5deffaf05dc9f15d3e55e
Luckisback <jlucbilo@gmail.com>
2023-02-01 15:34:01 +0100

    A function creat a class called Square whith -size as attribut

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Definition of Square as a class"""
+
+
+class Square:
+    """"Class Square that able tu compute a dimentions"""
+    def __init__(self, _size):
+        self._size = _size
