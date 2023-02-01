cb393220b0cff828021a302be3ff9812b817cccb
Luckisback <jlucbilo@gmail.com>
2023-02-01 18:08:17 +0100

    fixing the issue of the variable type

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,6 +5,8 @@
 class Square:
     """ Definition of class Square """
     def __init__(self, size: int = 0):
+        if type(size) != int:
+            raise typeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
