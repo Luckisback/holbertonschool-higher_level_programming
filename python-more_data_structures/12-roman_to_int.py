b57f9c04cd15d6279c33a101da81fa582e993170
Luckisback <jlucbilo@gmail.com>
2023-02-01 10:40:21 +0100

    consideration of mandatory conditions.

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_map = {'I': 1, 'V': 5,
+	         'X': 10, 'L': 50,
+		 'C': 100, 'D': 500,
+		 'M': 1000}
     result = 0
+    if type(roman_string) == str or roman_string == None:
+        return 0
     for i in range(len(roman_string)):
         curr = roman_map[roman_string[i]]
         if i+1 < len(roman_string) and roman_map[roman_string[i+1]] > curr:
