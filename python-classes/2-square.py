38efd15f77d4f7230303fa98cd88e1d95263fbf3
Luckisback <jlucbilo@gmail.com>
2023-02-01 17:50:57 +0100

    A programm that creats Class Square based on 1-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Creation of a Class called Square """
+
+
+class Square:
+    """ Definition of class Square """
+    def __init__(self, size: int = 0):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
