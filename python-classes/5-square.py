b4ef9dbb8e7c8b887a333d4890d33b89cb75ce49
Luckisback <jlucbilo@gmail.com>
2023-02-03 17:55:15 +0100

    removing some unecessaries lines

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -6,10 +6,6 @@ class Square:
     """ Declaration of Square """
     def __init__(self, size=0):
         self.__size = size
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
 
     def area(self):
         return self.__size ** 2
