0f85766eb8c3b28bb7a2d92457967bf2cef82591
Luckisback <jlucbilo@gmail.com>
2023-02-03 10:58:12 +0100

    fixing the number of blank line missed

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Declaration of a class"""
+
+
 class Square:
     """Definition of the class Square"""
     def __init__(self, size: int = 0):
@@ -8,7 +10,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
 
     def area(self):
         """ Definition of a public instance methode
