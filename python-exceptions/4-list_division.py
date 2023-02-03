772528b94f72c7d55e802aa5c0fac339f18b1f47
Luckisback <jlucbilo@gmail.com>
2023-02-03 16:19:02 +0100

    changing the priority of printing the 'exception message'

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,9 +7,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             result = 0
-        except TypeError:
-            print("wrong type")
-            result = 0
         except ZeroDivisionError:
             print("division by zero")
             result = 0
