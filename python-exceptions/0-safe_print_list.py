40c59a89f000e1e217de95acf4589628b1e3e65c
Luckisback <jlucbilo@gmail.com>
2023-02-03 14:57:24 +0100

    fixing a pb of white space

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     n = 0
     try:
-        for i in range(x) :
+        for i in range(x):
             print("{}".format(my_list[i]), end="")
             n += 1
         print()
