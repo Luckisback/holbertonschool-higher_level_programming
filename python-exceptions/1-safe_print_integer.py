1e04215cf7302493ba89b40ff8790acc58006501
Luckisback <jlucbilo@gmail.com>
2023-02-03 15:08:29 +0100

    dealing with the printing of nw line

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 def safe_print_integer(value):
     try:
         if isinstance(value, int):
-            print("{:d}".format(value), end=" ")
+            print("{:d}".format(value))
+            print()
             return True
     except:
         return False
