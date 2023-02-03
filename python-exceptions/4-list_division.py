1fdc5d7068ba4dcf29770d16277c8035e01969aa
Luckisback <jlucbilo@gmail.com>
2023-02-03 16:48:16 +0100

    changing the position of the 'last return'

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new_tab = []
+    new_list = []
     for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            print("division by zero")
+            print("division by 0")
             result = 0
         except TypeError:
             print("wrong type")
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             result = 0
         finally:
-            new_tab.append(result)
-        return new_tab
+            new_list.append(result)
+    return new_list
