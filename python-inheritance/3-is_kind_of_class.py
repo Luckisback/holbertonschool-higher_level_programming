f6c74dbfaa592d34420eb74553fa5d329d4afab1
Luckisback <jlucbilo@gmail.com>
2023-02-08 11:50:59 +0100

    fixing the output

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 """Declation of a function that checks a kind of class"""
+
+
 def is_kind_of_class(obj, a_class):
+    """ the function is_kind_of_class """
     return (True if type(obj) is a_class or isinstance(obj, a_class) else False)
