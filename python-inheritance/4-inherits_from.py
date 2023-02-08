f712aff97076afb4aa0ffbe7d0592500f9363489
Luckisback <jlucbilo@gmail.com>
2023-02-08 12:14:44 +0100

    a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Declaration of a fucntion """
+
+
+def inherits_from(obj, a_class):
+    """ A function that find the kind of type the class """
+    if issubclass(obj, a_class) and isinstance(obj, a_class):
+        return True
+    else:
+        return False
