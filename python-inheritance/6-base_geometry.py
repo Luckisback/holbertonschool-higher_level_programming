c9bd88f9ff6a36c8c42772ca873e7ae946c08ee4
Luckisback <jlucbilo@gmail.com>
2023-02-08 14:01:33 +0100

    A class that raise an exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defintion of a class """
+
+
+class BaseGeometry:
+    """ A class that raise an exeption """
+    def area(self):
+        """ definition of a methode of this class """
+        raise Exception("area() is not implemented")
