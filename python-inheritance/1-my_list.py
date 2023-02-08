3dcf26b5112ae65f4cb3f2a13c169be37b553db8
Luckisback <jlucbilo@gmail.com>
2023-02-08 09:34:01 +0100

    A class MyList that inherits from list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Declaration of class that inherits from a list"""
+class MyList(list):
+    """ Declaration of the function"""
+    def __init__(self):
+        super().__init__(self)
+
+    def print_sorted(self):
+        print(sorted(self))
