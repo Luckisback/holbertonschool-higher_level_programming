beefe03833147d319ff9d923a1beb48b230b73ea
Luckisback <jlucbilo@gmail.com>
2023-02-08 11:25:03 +0100

    fixing the doc issues

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ Declaration of a function that check type of an instance"""
+
+
 def is_same_class(obj, a_class):
+    """ Defintion of the class is_same_class """
     if isinstance(obj, a_class):
         return True
     return False
