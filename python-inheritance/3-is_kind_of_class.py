77e71cce288fc21e1cac4f3c3ac4d904914630da
Luckisback <jlucbilo@gmail.com>
2023-02-08 10:37:55 +0100

    a function that returns the kind of an object instance

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Declation of a function that checks a kind of class"""
+def is_kind_of_class(obj, a_class):
+    return (True if type(obj) is a_class or isinstance(obj, a_class) else False)
