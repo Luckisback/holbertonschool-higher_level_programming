c62b03b6f44842a2db49d4af6bf6a18d85c8e9f0
Luckisback <jlucbilo@gmail.com>
2023-02-13 14:49:49 +0100

    the class Rectangle that inherits from Base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,8 +7,6 @@ from models.base import Base
 
 class Rectangle(Base):
     """ definition of the methode __init__"""
-
-
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
