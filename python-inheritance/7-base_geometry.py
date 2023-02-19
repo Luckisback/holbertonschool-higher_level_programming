b59e6ce5d3efbf6a9785dacfd6835b7b8b86bba3
Luckisback <jlucbilo@gmail.com>
2023-02-19 23:21:35 +0100

    New version of class based on 6-base-geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -3,15 +3,17 @@
 
 
 class BaseGeometry:
-    """ A classe that raise with an exception """
+    """class empty"""
+
     def area(self):
-        """ A function as method to raise """
-        raise Exception("area() is not implemented")
+        """Raises an exception"""
+        raise Exception('area() is not implemented')
+
 
     def integer_validator(self, name, value):
-        """ A methode that checks if Int """
-        if type(name) is str:
-            if type(value) != int:
-                raise TypeError("<name> must be an integer")
-            if value <= 0:
-                raise ValueError("<name> must be greater than 0")
+        """Fonction Raises an exception"""
+
+        if not type(value) is int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
