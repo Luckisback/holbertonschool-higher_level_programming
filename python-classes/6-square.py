650c47b809fde2d0920e4e09d60d27eae8ace328
Luckisback <jlucbilo@gmail.com>
2023-02-19 22:51:56 +0100

    last verion of le class called Square based on 5-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -73,4 +73,4 @@ class Square:
                     print(" ", end="")
                 for j in range(self.size):
                     print("#", end="")
-                print()        
+                print()
