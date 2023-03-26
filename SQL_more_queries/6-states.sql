0909e7621661ad75f0c4b386477baf680d4eabbd
Luckisback <jlucbilo@gmail.com>
2023-03-27 01:58:21 +0200

    A script that creates the database "hbtn_0d_usa" and the table "cities"

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- A script that creates the database "hbtn_0d_usa" and the table "states"
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE states (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256)
+);
