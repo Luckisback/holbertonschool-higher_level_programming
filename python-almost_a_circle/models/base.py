55874b6bfc2ba63418a5fcc8d43d3d31d374a73f
Jean-Luc <jlucbilo@gmail.com>
2023-10-26 15:35:38 +0200

    fixing pycodesyle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-""" 
+"""
 Base Class
 
 Args:
     Base: First class
     __nb_objects: the private class attribut
-    id: A public instance attribut    
+    id: A public instance attribut
 """
+
 
 class Base:
     """ The first class called base """
-
     __nb_objects = 0
+
     def __init__(self, id=None):
+
         if id is not None:
             self.id = id
         else:
