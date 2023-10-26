7a4813c190307e0073b4230382e68dcac0240156
Jean-Luc <jlucbilo@gmail.com>
2023-10-26 15:23:02 +0200

    fixing pravate instance attributs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -32,10 +32,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
 # Setting of the Rectangle's instance attribute behaviors
     @property
