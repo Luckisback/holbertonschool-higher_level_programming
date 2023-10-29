4291b5d74557607d6498b52a05a30d44bb919bf1
Jean-Luc <jlucbilo@gmail.com>
2023-10-29 12:55:23 +0100

    Thae calss MyInt that inherit fromm Int

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""The class that inherites from int"""
+
+
+class MyInt(int):
+    """class child of Int"""
+    def __init__(self, '==', '!='):
+        self.'==' = '=='
+        self.'!=' = '!='
