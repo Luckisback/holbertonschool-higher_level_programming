02bdfb5bd431b2db1614e7d2e63f0a7e4cc5c087
Jean-Luc <jlucbilo@gmail.com>
2023-10-29 19:57:00 +0100

    dealling with pycodesyle errors

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Write a function tha creats an objet in a file with json"""
 
+
+"""Write a function tha creats an objet in a file with json"""
 
 import json
 
 
-def save_to_json_file(my_obj, filename)::
+def save_to_json_file(my_obj, filename):
     """A function that save in json format"""
-      with open(filename, 'w', encoding='utf8') as f:
+    with open(filename, 'w', encoding='utf8') as f:
         json.dump(my_obj, f)
