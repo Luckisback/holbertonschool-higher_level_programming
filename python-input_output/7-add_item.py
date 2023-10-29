09e376fdafdeb3dac87b2b79fc93354c0f8a5ba9
Jean-Luc <jlucbilo@gmail.com>
2023-10-29 22:28:33 +0100

    a script that adds all arguments to a Python list, and then save them to a JSON file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python3
 """Write a script that adds all arguments to a Python list"""
 
-
-import sys
 import json
+import os
+import sys
+import importlib
 
+# Import the 'save_to_json_file' function using importlib
+save_to_json_file_module = importlib.import_module('5-save_to_json_file')
+save_to_json_file = save_to_json_file_module.save_to_json_file
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+# Import the 'load_from_json_file' function using importlib
+load_from_json_file_module = importlib.import_module('6-load_from_json_file')
+load_from_json_file = load_from_json_file_module.load_from_json_file
 
-try:
-    my_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    my_list = []
+def main():
+    # Get the command-line arguments (excluding the script name)
+    args = sys.argv[1:]
 
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
+    # Initialize existing_data to an empty list
+    existing_data = []
 
-save_to_json_file(my_list, "add_item.json")
+    # If the file "add_item.json" exists, load its contents
+    if os.path.exists("add_item.json"):
+        try:
+            existing_data = load_from_json_file("add_item.json")
+        except (FileNotFoundError, json.JSONDecodeError):
+            pass
+
+    # Add the command-line arguments to the list
+    existing_data.extend(args)
+
+    # Save the updated list to "add_item.json"
+    save_to_json_file(existing_data, "add_item.json")
+
+if __name__ == "__main__":
+    main()
