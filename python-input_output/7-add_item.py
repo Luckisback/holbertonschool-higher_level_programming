bfd302677753b3ee61634d1ba8635787844762c8
Jean-Luc <jlucbilo@gmail.com>
2023-10-29 22:36:22 +0100

    fixing pycodestyle errors

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,33 +6,35 @@ import os
 import sys
 import importlib
 
-# Import the 'save_to_json_file' function using importlib
+""" Import the 'save_to_json_file' function using importlib"""
 save_to_json_file_module = importlib.import_module('5-save_to_json_file')
 save_to_json_file = save_to_json_file_module.save_to_json_file
 
-# Import the 'load_from_json_file' function using importlib
+""" Import the 'load_from_json_file' function using importlib"""
 load_from_json_file_module = importlib.import_module('6-load_from_json_file')
 load_from_json_file = load_from_json_file_module.load_from_json_file
 
+
 def main():
-    # Get the command-line arguments (excluding the script name)
+    """ Get the command-line arguments (excluding the script name)"""
     args = sys.argv[1:]
 
-    # Initialize existing_data to an empty list
+    """ Initialize existing_data to an empty list """
     existing_data = []
 
-    # If the file "add_item.json" exists, load its contents
+    """ If the file "add_item.json" exists, load its contents"""
     if os.path.exists("add_item.json"):
         try:
             existing_data = load_from_json_file("add_item.json")
         except (FileNotFoundError, json.JSONDecodeError):
             pass
 
-    # Add the command-line arguments to the list
+    """ Add the command-line arguments to the list"""
     existing_data.extend(args)
 
-    # Save the updated list to "add_item.json"
+    """ Save the updated list to 'add_item.json' """
     save_to_json_file(existing_data, "add_item.json")
+
 
 if __name__ == "__main__":
     main()
