b22891be439ebb89df1a44c120e48d40907c0067
Jean-Luc <jlucbilo@gmail.com>
2023-11-20 15:18:37 +0100

    fixing syntax error

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,8 @@
 -- A script that creates a table: second_table
 
 CREATE TABLE IF NOT EXISTS second_table (
-    id int,
+    id INT,
     name VARCHAR(256),
-    score int
+    score INT
 );
-INSERT INTO second_table (id, name, score) 
-VALUES (1, "john", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES (1, "john", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
