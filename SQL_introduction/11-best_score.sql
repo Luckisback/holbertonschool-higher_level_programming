ed9d2c82bbe231eff155870587c7747fc4b63ab4
Jean-Luc <jlucbilo@gmail.com>
2023-11-20 15:32:26 +0100

    fixing the name of the database 'table

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- A script that lists all records with a score >= 10
 
-SELECT score, name FROM seconde_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
