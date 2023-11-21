508daf2f35dd4eacec5a47c3a51d954836f83f17
Jean-Luc <jlucbilo@gmail.com>
2023-11-21 18:28:25 +0100

    A script that creates the table with a primary key unique key not null

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- A script that creates the database "hbtn_0d_usa" and the table "states"
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE states (
-    PRIMARY KEY (id),
-    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL
 );
