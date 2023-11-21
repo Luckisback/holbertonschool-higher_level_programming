57512026ff9c494b9becfa5a17ec1f417a0262e8
Jean-Luc <jlucbilo@gmail.com>
2023-11-21 23:08:03 +0100

    fixing syntax error 'REFERENCE(S)'

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -8,4 +8,4 @@ CREATE TABLE IF NOT EXISTS cities (
 		id INT UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL,
 		name VARCHAR(256) NOT NULL,
 		state_id INT NOT NULL, 
-		FOREIGN KEY (state_id) REFERENCE states (id));
+		FOREIGN KEY (state_id) REFERENCES states (id));
