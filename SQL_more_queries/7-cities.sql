ad333fbb086412938778c10f0d76d548ec4549ff
Jean-Luc <jlucbilo@gmail.com>
2023-11-21 22:57:38 +0100

    Adding the missing instruction 'NOT NULL' for the foreign key

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -7,4 +7,5 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 		id INT UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL,
 		name VARCHAR(256) NOT NULL,
-		state_id INT FOREIGN KEY REFERENCE states (id));
+		state_id INT NOT NULL, 
+		FOREIGN KEY (state_id) REFERENCE states (id));
