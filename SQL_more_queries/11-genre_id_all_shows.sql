fa8642cc5eefdf6a0346a85b4733fc437286875a
Jean-Luc <jlucbilo@gmail.com>
2023-11-22 21:54:37 +0100

    a script that lists all shows contained in the database

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
--- a script that lists all shows contained in the database
+-- A script that lists all shows contained in the database
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_shows_genres.genre_id
+ON tv_shows.id = tv_shows_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
