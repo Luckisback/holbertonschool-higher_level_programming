0c1c66ba7f9592bd7589166ccd0e463620a81ec5
Jean-Luc <jlucbilo@gmail.com>
2023-11-22 20:17:28 +0100

    a script that converts database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -2,7 +2,6 @@
 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; 
 
-USE hbtn_0c_0;
 -- modifying the character set and collation of the entire first_table
 
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
