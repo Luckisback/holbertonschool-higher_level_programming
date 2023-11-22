748bec7ed39167ffc20e2fc19fd72377d4f6eec0
Jean-Luc <jlucbilo@gmail.com>
2023-11-22 12:29:16 +0100

    fixing queries issues

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 --a script that lists all the cities coming from an other table
 
 SELECT id, name FROM cities
-WHERE state_id = (SELECT id FROM states WHERE id = 1 AND name = "California";),
+WHERE state_id = (SELECT id FROM states WHERE name = "California"),
 ORDER BY id ASC; 
