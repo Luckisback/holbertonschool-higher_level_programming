1e156b842d8bd036aa5fe6de627e5a2925cfe45d
Jean-Luc <jlucbilo@gmail.com>
2023-11-22 14:01:40 +0100

    Commi,g back on my old cod

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,5 @@
 --a script that lists all the cities coming from an other table
 
---SELECT id, name FROM cities
---WHERE state_id = (SELECT id FROM states WHERE name = "California"),
---ORDER BY id ASC; 
-
-SELECT id, name FROM cities WHERE state_id = 1 ORDER BY cities.id ASC;
+SELECT id, name FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = "California"),
+ORDER BY id ASC; 
