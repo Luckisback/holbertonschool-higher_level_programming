f3c7bf0de3da5c5ce1155bc4c923f3a416e200be
Jean-Luc <jlucbilo@gmail.com>
2023-11-22 16:15:22 +0100

    -- Removing the statement 'Left' of JOIN instruction

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 --A script that lists all cities contained in a database
 
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states 
+SELECT cities.id, cities.name, states.name FROM cities JOIN states 
 	ON cities.state_id = states.id
 	ORDER BY cities.id ASC;
