4a1d3b543b625e30d0e99476419619b7130edfb5
Jean-Luc <jlucbilo@gmail.com>
2023-11-22 16:43:05 +0100

    -- fixing syntax errors

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
---A script that lists all cities contained in a database
+-- A script that lists all cities contained in a database
 
 SELECT cities.id, cities.name, states.name
-FROM cities JOIN states ON cities.state_id = states.id
+FROM cities JOIN states 
+ON cities.state_id = states.id
 ORDER BY cities.id ASC;
