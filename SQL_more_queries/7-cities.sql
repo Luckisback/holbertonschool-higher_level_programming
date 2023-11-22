baddb2b96c669ca5f47035e338a3992b9ca7fbe8
Jean-Luc <jlucbilo@gmail.com>
2023-11-22 12:01:15 +0100

    fixing queries

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -6,6 +6,5 @@ USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities ( 
 		id INT UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL, 
-		state_id INT NOT NULL, 
-		FOREIGN KEY (state_id) REFERENCES states (id)
+		state_id INT NOT NULL FOREIGN KEY (state_id) REFERENCES states (id)
 		name VARCHAR(256) NOT NULL,);
