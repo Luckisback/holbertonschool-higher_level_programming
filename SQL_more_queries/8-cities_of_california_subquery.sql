03811cf92582d0d82475e1c69433477eb6c4167c
Jean-Luc <jlucbilo@gmail.com>
2023-11-22 11:47:50 +0100

    fixing the nested queries

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 --a script that lists all the cities coming from an other table
 
-SELECT state_id, name FROM cities
+SELECT id, name FROM cities
 WHERE state_id = (
 		SELECT id FROM states
 		WHERE name = 'California')
-ORDER BY cities;
+ORDER BY id;
