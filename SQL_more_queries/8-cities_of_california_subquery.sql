1f8fcb537743c2609ec7830094219c03a5969358
Jean-Luc <jlucbilo@gmail.com>
2023-11-22 16:47:42 +0100

    fixing syntax errors

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- A script that lists all the cities coming from an other table
 
 SELECT id, name FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = "California"),
+WHERE state_id = (SELECT id FROM states WHERE name = "California")
 ORDER BY id ASC; 
