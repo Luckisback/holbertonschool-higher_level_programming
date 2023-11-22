c79d4b55f962d8135d9a656c8057476df7dcee74
Jean-Luc <jlucbilo@gmail.com>
2023-11-22 20:30:17 +0100

    fixing syntax errors

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,9 @@
--- a script that converts hbtn_0c_0 database to UTF8
--- modify the character set and collation of the name in the field in the first_t    able
--- modifying the character set and collation of the entire first_table
--- ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_c    i;
--- ALTER TABLE first_table MODIFY COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLL    ATE utf8mb4_unicode_ci;
+-- A script that converts hbtn_0c_0 database to UTF8
 
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; 
+ALTER DATABASE hbtn_0c_0 
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; 
+ALTER TABLE first_table 
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table 
+MODIFY COLUMN name VARCHAR(255) 
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
