e32496bc5423fdcc731b361fe4657892514cef09
Jean-Luc <jlucbilo@gmail.com>
2023-11-27 20:58:55 +0100

    detetion of some comments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,20 +6,11 @@ import MySQLdb
 
 if __name__ == "__main__":
 
-    """connection to the dabtabase """
     con = MySQLdb.connect(host="localhost", port=3306, user="root", password="", database="hbtn_0e_0_usa")
     cur = con.cursor()
-
-    """ The query for retieving information from the table """
     cur.execute("SELECT * FROM states ORDER BY states.id")
-
-    """ Information recovery """
     result = cur.fetchall()
-
-    """ listing information"""
     for _ in result:
         print(_)
-
-    """ Closing the connection """
     cur.close()
     con.close()
