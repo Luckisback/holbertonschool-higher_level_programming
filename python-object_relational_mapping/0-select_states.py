575664f18c4832c1be19d001915a925d729bc841
Jean-Luc <jlucbilo@gmail.com>
2023-11-27 21:03:06 +0100

    fixing syntax error

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import MySQLdb
 
 if __name__ == "__main__":
 
-    con = MySQLdb.connect(host="localhost", port=3306, user="root", password="", database="hbtn_0e_0_usa")
+    con = MySQLdb.connect(host='localhost', port=3306, user='root', password='', database='hbtn_0e_0_usa')
     cur = con.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id")
     result = cur.fetchall()
