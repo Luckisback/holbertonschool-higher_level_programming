7d03e3a475bb7e6d441d8af6b4604255e3944608
Jean-Luc <jlucbilo@gmail.com>
2023-11-27 23:32:28 +0100

    fixing issues

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 
+""" A script that lists all states with a name starting with upper 'N'  """
+
 import MySQLdb
 import sys
 
-""" A script that lists all states with a name starting with upper 'N'  """
-
 if __name__ == "__main__":
 
-    h = "localhost"
-    p = 3306
     u = sys.argv[1]
     pwd = sys.argv[2]
     db = sys.argv[3]
 
-    con = MySQLdb.connect(host=h, port=p, user=u, password=pwd, database=db)
-    cur = con.cursor()
+    con = MySQLdb.connect(user=u, password=pwd,
+                                 database=db, host="localhost",
+                                 port=3306)
 
-    req = "SELECT * FROM states ORDER BY states.id")
+    cur = connection.cursor()
+    req = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id"
     cur.execute(req)
 
-    result = cur.fetchall()
+    res_tup = cur.fetchall()
 
-    for eta in result:
-        if sta[1][0] == "N":
-            print(eta)
+    for eta in stateList:
+        print(eta)
 
-    cur.close()
-    con.close()
+    cursor.close()
+    connection.close()
