f53afa80c89dd71f8d478218b9d3ba979ab31a9b
Jean-Luc <jlucbilo@gmail.com>
2023-11-27 21:53:10 +0100

    removing some coments

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,26 +7,21 @@ import sys
 
 if __name__ == "__main__":
 
-    """ Declaration of connection's parameters """
     h = "localhost"
     p = 3306
-    u = argv[1]
-    pwd = argv[2]
-    db = argv[3]
+    u = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
 
-    """ Connection to the database """
     con = MySQLdb.connect(host=h, port=p, user=u, password=pwd, database=db)
     cur = con.cursor()
 
-    """ Recovering information from a table """
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
 
-    """ Displaying the retrieved contant """
     result = cur.fetchall()
 
     for _ in result:
         print(_)
 
-    """ Closing database's connection """
     cur.close()
     con.close()
