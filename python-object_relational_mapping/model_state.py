8cd12513ae82f240008f7ddc84b24d3fce879151
Jean-Luc <jlucbilo@gmail.com>
2023-11-28 16:06:41 +0100

    changing some lines in the code

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,28 +5,16 @@
 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Colum, Integer, String, Auto_increment, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sys import argv
 
-if __name__ == "__main__":
+Base = declarative_base()
 
-    Base = declarative_base()
-    
-    u = argv[1]
-    mdp = argv[2]
-    h = "localhost"
-    p = 3306
-    bdd = argv[3]
+class Cities(Base):
 
+    __tablename__='States'
 
-    class Cities(Base):
-
-        __tablename__='States'
-
-        id = column(Integer, urimary_Key=True, unique=True, nullable=False, autoincrement=True)
-        name = column(String(128), nullable=False)
-
-        engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(u, mdp, h, p, bdd, pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
+    id = Column("id", Integer, primary_key=True, unique=True, nullable=False, autoincrement=True)
+    name = Column("name", String(128), nullable=False)
