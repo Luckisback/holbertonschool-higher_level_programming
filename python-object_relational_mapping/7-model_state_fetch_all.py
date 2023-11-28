ac3ca4399d045b03806e8d34b003fa291bd1c0d5
Jean-Luc <jlucbilo@gmail.com>
2023-11-28 16:57:36 +0100

    code with explicite parametres, instead of variables

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,10 +13,8 @@ if __name__ == "__main__":
     u = sys.argv[1]
     p = sys.argv[2]
     bdd = sys.argv[3]
-    h = "localhost"
-    p = 3306
 
-    engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(u, pw, h, p, bdd))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(u, pw, h, p, bdd))
 
     result = engine.execute(text("SELECT * FROM states;"))
     result.fetchall()
