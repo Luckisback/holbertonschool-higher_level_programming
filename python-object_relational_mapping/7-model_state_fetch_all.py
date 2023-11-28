d95cccfb7a67cd3d70814577824ce6d848d0c16e
Jean-Luc <jlucbilo@gmail.com>
2023-11-28 17:00:05 +0100

    code with explicite parametres, instead of variables

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,15 +6,15 @@ This code lists all State objects from the database hbtn_0e_6_usa
 from model_state import Base, State
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
-import sys
+from sys import argv
 
 if __name__ == "__main__":
 
-    u = sys.argv[1]
-    p = sys.argv[2]
-    bdd = sys.argv[3]
+    u = argv[1]
+    p = argv[2]
+    bdd = argv[3]
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(u, pw, h, p, bdd))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(u, p, bdd))
 
     result = engine.execute(text("SELECT * FROM states;"))
     result.fetchall()
