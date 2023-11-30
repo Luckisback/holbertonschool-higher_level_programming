8f81c8ddb7fe69f969f65bbd529368578cf93498
Jean-Luc <jlucbilo@gmail.com>
2023-11-30 16:03:46 +0100

    creating table from ORM with python script

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,13 +9,14 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class States(Base):
+class State(Base):
     """ Creation of the table States in the database hbtn_0e_6_usa """
 
-    __tablename__ = 'States'
+    __tablename__ = 'states'
 
     id = Column(Integer,
                 primary_key=True,
-                nullable=False)
+                nullable=False,
+		autoincrement=True)
 
     name = Column(String(128), nullable=False)
