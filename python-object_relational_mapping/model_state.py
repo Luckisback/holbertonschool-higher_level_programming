a2730f01950d44ebc9cce7f04bbc396310658dc9
Jean-Luc <jlucbilo@gmail.com>
2023-11-30 15:13:49 +0100

    fixing some issues

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 
-""" Start link class to table in database """
+""" linked the database wiht the code """
+
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class States(Base):
     """ Creation of the table States in the database hbtn_0e_6_usa """
 
-    __tablename__='States'
+    __tablename__ = 'States'
 
-    id = Column(Integer, primary_key=True, unique=True, nullable=False, autoincrement=True)
+    id = Column(Integer,
+                primary_key=True,
+                unique=True,
+                nullable=False,
+                autoincrement=True)
+
     name = Column(String(128), nullable=False)
