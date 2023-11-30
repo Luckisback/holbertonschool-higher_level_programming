3e70e857e0a7400a35daf3280f4f8c79be1eb327
Jean-Luc <jlucbilo@gmail.com>
2023-11-30 12:20:46 +0100

    checking equality beween the table's value and the arguments

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,6 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    """ Declaration for connction to the database """
     h = "localhost"
     p = 3306
     u = argv[1]
@@ -15,14 +14,13 @@ if __name__ == "__main__":
     db = argv[3]
     nom_stat = argv[len(argv) - 1]
 
-    """ Connection to the database """
     con = MySQLdb.connect(host=h,
                           port=p, user=u,
                           password=pwd,
                           database=db)
     cur = con.cursor()
     Qry = "SELECT * FROM states\
-        WHERE name = BINARY %s ORDER BY states.id".format(nom_stat)
+        WHERE name = BINARY {} ORDER BY states.id".format(nom_stat)
     cur.execute(Qry)
     q_result = cur.fetchall()
 
