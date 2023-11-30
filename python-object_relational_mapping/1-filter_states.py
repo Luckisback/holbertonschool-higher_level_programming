13d5d5f7a3cb13d792b5da0323d20324c961a092
Jean-Luc <jlucbilo@gmail.com>
2023-11-30 09:38:54 +0100

    fixing pycodestyle errors

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     pwd = sys.argv[2]
     db = sys.argv[3]
 
-    con = MySQLdb.connect(host="localhost",port=3306, user=u, password=pwd, database=db)
+    con = MySQLdb.connect(host="localhost", port=3306,
+                          user=u, password=pwd, database=db)
 
     cur = con.cursor()
     req = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id"
