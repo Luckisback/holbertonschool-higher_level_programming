790b8c48bef046d4e267e0c82b0d81ffb8e25f0e
Jean-Luc <jlucbilo@gmail.com>
2023-11-30 13:56:12 +0100

    fixing indentations problems

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """ a script that takes in the name of a state as an argument
-    and lists all cities of that state 
+    and lists all cities of that state
 """
 
 import MySQLdb
@@ -14,12 +14,13 @@ if __name__ == "__main__":
     u = argv[1]
     mdp = argv[2]
     bdd = argv[3]
-    nom_stat = argv[len(argv) -1]
+    nom_stat = argv[len(argv) - 1]
 
     """ Connection to the database """
     con = MySQLdb.connect(host=h, port=p, user=u, password=mdp, database=bdd)
     cur = con.cursor()
-    cur.execute("SELECT cities.name, states.name FROM cities JOIN states ON states.id = cities.state_id ORDER BY cities.id")
+    cur.execute("SELECT cities.name, states.name FROM\
+        cities JOIN states ON states.id = cities.state_id ORDER BY cities.id")
     res = cur.fetchall()
     disp_list = []
 
