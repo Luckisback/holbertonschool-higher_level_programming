59a8f30d1089470025cff20ea8bb695fc51c64c1
Jean-Luc <jlucbilo@gmail.com>
2023-12-01 16:50:18 +0100

    fixing some issues

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,21 +3,21 @@
 """ The script that prints the State object with the name passed as argument """
 
 from model_state import Base, State
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 if __name__ == "__main__":
 
-    us = sys.argv[1]
-    pas = sys.argv[2]
-    dbase = sys.argv[3]
+    us = argv[1]
+    pas = argv[2]
+    dbase = argv[3]
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
-                           format(us, pas, dbase))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                            .format(us, pas, dbase))
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    stateUpdate = session.query(State).filter(State.id == 2).first()
-    stateUpdate.name = "New Mexico"
+    session.query(State).filter(State.id == 2).update({"name": "New Mexico"})
     session.commit()
+    session.close()
