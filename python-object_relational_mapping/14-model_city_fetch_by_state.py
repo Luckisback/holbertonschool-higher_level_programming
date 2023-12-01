6610c2460583ea9560db19eb9c235528b3454240
Jean-Luc <jlucbilo@gmail.com>
2023-12-01 17:06:54 +0100

    fixing some syntax errors

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-""" Prints all City objects from the database hbtn_0e_14_usa"""
+""" Prints all City objects from
+the database hbtn_0e_14_usa """
 
-import sys
+from sys import argv
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
@@ -10,18 +11,20 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
 
-    us = sys.argv[1]
-    paw = sys.argv[2]
-    dtb = sys.argv[3]
+    us = argv[1]
+    paw = argv[2]
+    dtb = argv[3]
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(us, paw, dtb), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(us, paw, dtb), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for s, c in session.query(State, City).\
-            filter(State.id == City.state_id).\
-            order_by(City.id):
+    for s, c in session.query(State, City)\
+        .filter(State.id == City.state_id)\
+            .order_by(City.id):
+
         print("{}: ({}) {}".format(s.name, c.id, c.name))
 
     session.close()
