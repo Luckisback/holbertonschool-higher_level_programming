a46443b516bcc3858e13378cc06ba888536ffe86
Jean-Luc <jlucbilo@gmail.com>
2023-12-01 14:42:14 +0100

    fixing some issues

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,15 +3,15 @@
 """ lists all State objects from the database hbtn_0e_6_usa"""
 
 from model_state import Base, State
-from sqlalchemy import create_engine, text
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 if __name__ == "__main__":
 
-    us = sys.argv[1]
-    pw = sys.argv[2]
-    db = sys.argv[3]
+    us = argv[1]
+    pw = argv[2]
+    db = argv[3]
 
     engine = create_engine("mysql+mysqldb://{}:{}\
         @localhost:3306/{}".format(us, pw, db), pool_pre_ping=True)
@@ -20,19 +20,10 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     """ retrieve the first state"""
-    state = session.query(State).order_by(State.id).first()
+    qy = session.query(State).filter\
+        (State.name.like('%a%')).order_by(State.id)
 
-    if state:
+    """ display the result """
+    for state in qy:
         print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
     session.close()
-
-    """
-    qry = engine.execute
-    (text("SELECT * FROM states ORDER BY states.id LIMIT 1;"))
-    result =qry.fetchall()
-
-    for row in result:
-        print("{}: {}".format(row[0], row[1]))
-    """
