d50c535289e27bfcf4e657a795c835ff8f7f2e41
Jean-Luc <jlucbilo@gmail.com>
2023-12-01 15:14:24 +0100

    fixing some issues

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 
-""" The script that prints the State object 
+""" The script that prints the State object
     with the name passed as argument """
 
 from model_state import Base, State
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 
 if __name__ == "__main__":
 
-    us = sys.argv[1]
-    pw = sys.argv[2]
-    bdd = sys.argv[3]
+    us = argv[1]
+    pw = argv[2]
+    bdd = argv[3]
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(us, pw, bdd))
+    engine = create_engine
+    ("mysql+mysqldb://{}:{}@localhost:3306/{}".format(us, pw, bdd))
 
     Session = sessionmaker(bind=engine)
     session = Session()
