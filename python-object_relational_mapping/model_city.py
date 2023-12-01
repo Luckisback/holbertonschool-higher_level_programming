fdf9ac26a4caf9a88b86b6801f126f8ce95d0b40
Jean-Luc <jlucbilo@gmail.com>
2023-12-01 20:21:35 +0100

    fixing some issues

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-
-""" Defines the class City """
-
-from sqlalchemy import Column, ForeignKey, Integer, String
+"""
+Class definition of a City and
+an instance Base = declarative_base()
+"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 Base = declarative_base()
 
 
 class City(Base):
-   """" Definition of the class Cities"""
-
-    __tablename__ = "cities"
-
-    id = Column(Integer, primary_key=True)
+    """
+    Defines ORM class for table `cities`,
+    with 3 columns id  name and state_id
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
