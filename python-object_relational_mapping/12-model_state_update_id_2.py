f7fa09a7fd94f87b58b8e960ea9aa3c287e07905
Jean-Luc <jlucbilo@gmail.com>
2023-12-01 16:53:28 +0100

    fixing some issues

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-""" The script that prints the State object with the name passed as argument """
+""" The script that prints the State object
+with the name passed as argument """
 
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -13,8 +14,8 @@ if __name__ == "__main__":
     pas = argv[2]
     dbase = argv[3]
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                            .format(us, pas, dbase))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost\
+                            :3306/{}".format(us, pas, dbase))
 
     Session = sessionmaker(bind=engine)
     session = Session()
